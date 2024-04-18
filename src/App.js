3fa9bd2bde0766b0be01f2e6606a8d93b96c4fe0
Mohamed Khalifa <mkhkun11@gmail.com>
2024-04-18 18:59:30 +0200

    add share buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import {
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  TelegramIcon,
+  WhatsappIcon,
+  XIcon,
+} from "react-share";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
+  console.log(window.location.href);
   return (
     <>
       <h1>Quote Generator React App</h1>
@@ -30,13 +41,25 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="share-btns">
+          <TelegramShareButton url={window.location.href} title={quote.content}>
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+          <TwitterShareButton url={window.location.href} title={quote.content}>
+            <XIcon size={32} round />
+          </TwitterShareButton>
+          <WhatsappShareButton url={window.location.href} title={quote.content}>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
